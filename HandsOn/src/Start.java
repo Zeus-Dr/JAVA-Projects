f2ef15beebfa26b0962ea64eb79e928ac647407b
Zeus <mwesigyejosephug0@gmail.com>
2021-09-16 09:24:46 +0300

    Initial commit

---
+++ HandsOn/src/Start.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Start {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+		Scanner in= new Scanner(System.in);
+		sy
+		
+		String name;
+		int age;
+		System.out.print("Enter ur name:");
+		name=in.nextLine();
+		System.out.println(name);
+		
+		System.out.println("Enter ur age:");
+		age=Integer.parseInt(in.nextLine());
+		System.out.println(age);
+		
+	}
+}
