f4fa884c63984980f7b07a088eb825eccd069cf1
Zeus <mwesigyejosephug0@gmail.com>
2021-09-22 13:55:49 +0300

    Day 9 Maze Solver

--- MazeSolver/src/Position.java
+++ MazeSolver/src/Position.java
@@ -2,7 +2,7 @@
 public class Position {
 	public int x;//Column
 	public int y;//Row
-	public Position(int x, int y) {
+	public Position(int x, int y) {//Constructor for positions.
 		this.x = x;
 		this.y = y;
 	}
--- MazeSolver/src/mazesolver.java
+++ MazeSolver/src/mazesolver.java
@@ -4,13 +4,14 @@ public class mazesolver {
 	//0->wall
 	//1->path
 	//2->destination
-	static int[][] maze= {
+	//We start from position 3,0.
+	static int[][] maze= {//4*4 maze
 			{2,0,1,1},
 			{1,1,0,1},
 			{0,1,1,1}
 	};
 	
-	static LinkedList<Position> path=new LinkedList<Position>();
+	static LinkedList<Position> path=new LinkedList<Position>();//
 	
 	public static void main(String[] args) {
 		Position p=new Position(3,0);
