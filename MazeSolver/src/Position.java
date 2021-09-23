90f7f788759628310047ba6223d7ac01284b96dd
Zeus <mwesigyejosephug0@gmail.com>
2021-09-23 13:39:15 +0300

    Day 10 Adding different directons to mazegit status

--- MazeSolver/src/Position.java
+++ MazeSolver/src/Position.java
@@ -2,7 +2,7 @@
 public class Position {
 	public int x;//Column
 	public int y;//Row
-	public Position(int x, int y) {//Constructor for positions.
+	public Position(int y, int x) {//Constructor for positions.
 		this.x = x;
 		this.y = y;
 	}
--- MazeSolver/src/mazesolver.java
+++ MazeSolver/src/mazesolver.java
@@ -7,16 +7,68 @@ public class mazesolver {
 	//We start from position 3,0.
 	static int[][] maze= {//4*4 maze
 			{2,0,1,1},
-			{1,1,0,1},
-			{0,1,1,1}
+			{1,1,1,0},
+			{0,0,0,1}
 	};
 	
+	//Store our paths in a stack data structure.
 	static LinkedList<Position> path=new LinkedList<Position>();//
 	
 	public static void main(String[] args) {
-		Position p=new Position(3,0);
+		Position p=new Position(0,3);
 		path.push(p);
-		maze[path.peek().y][path.peek().x]=0;
+		
+		while(true) {
+			int y=path.peek().y;
+			int x=path.peek().x;
+			
+			maze[y][x]=0;//Blocking previous path.
+			
+			//down
+			if (maze[y+1][x]==2) {
+				System.out.println("Moved down.\nYou won!");
+				return;
+			}else if(maze[y+1][x]==1) {
+				System.out.println("Moved down.");
+				path.push(new Position(y+1, x));
+				continue;
+			}
+			
+			//left
+			if (maze[y][x-1]==2) {
+				System.out.println("Moved left.\nYou won!");
+				return;
+			}else if(maze[y][x-1]==1) {
+				System.out.println("Moved left.");
+				path.push(new Position(y, x-1));
+				continue;
+			}
+			
+			//right
+			if (maze[y][x+1]==2) {
+				System.out.println("Moved right.\nYou won!");
+				return;
+			}else if(maze[y][x+1]==1) {
+				System.out.println("Moved right.");
+				path.push(new Position(y, x+1));
+				continue;
+			}
+			
+			//up
+			if (maze[y-1][x]==2) {
+				System.out.println("Moved up.\nYou won!");
+				return;
+			}else if(maze[y-1][x]==1) {
+				System.out.println("Moved up.");
+				path.push(new Position(y-1, x));
+				continue;
+			}
+			path.pop();
+			if (path.size()<=0) {
+				System.out.println("No path!!");
+			}
+		}
+
 	}
 
 }
